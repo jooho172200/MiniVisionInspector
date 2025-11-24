6823268aa6a7b7b667b2ac47e1dff461f3de7bca
jooho172200 <jooho172200@gmail.com>
2025-11-24 14:32:22 +0900

    초기 커밋.

---
+++ MainForm.cs
@@ -0,0 +1,15 @@
+namespace MiniVIsionInspector
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
